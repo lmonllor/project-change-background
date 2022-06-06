4cab8440741d0237afc6cba573056d0447829ede
Ludmila Ailen Monllor <ludmilamonllordg@gmail.com>
2022-06-06 20:44:04 -0300

    1er trabajo color

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function changebackground(){
+    const symbols="0123456789ABCDEF";
+    let color="#";
+    for (let i=0;i<6;i++){
+        color=color+symbols[Math.floor(Math.random()*16)];  
+    }
+  document.body.style.background=color;
+  document.querySelector("span").innerHTML=color;
+    }
